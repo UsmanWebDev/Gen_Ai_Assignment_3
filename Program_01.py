f3775ce722c9d12b11bd18737e4e88107a4269c2
Muhammad Usman <usmanwebdevelopmentwithreactjs@gmil.com>
2024-08-21 08:18:32 +0000

    Calculate your age based on the current year and your birth year.

---
+++ Program_01.py
@@ -0,0 +1,11 @@
+# Calculate your age based on the current year and your birth year.
+
+# Ask for birth year and current year
+birth_year = int(input("Enter your birth year: "))
+current_year = int(input("Enter the current year: "))
+
+# Calculate age
+age = current_year - birth_year
+
+# Print age
+print("Your age is:", age)
