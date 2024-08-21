e189c66e2f10f945209025452b0c0be36544fb1e
Muhammad Usman <usmanwebdevelopmentwithreactjs@gmil.com>
2024-08-21 08:29:17 +0000

    calculates the area of a rectangle

---
+++ Program_02.py
@@ -0,0 +1,12 @@
+# calculates the area of a rectangle
+
+
+# Define length and width variables
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area
+area = length * width
+
+# Print the area
+print("The area of the rectangle is:", area)
